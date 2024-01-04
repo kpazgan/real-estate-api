c9471618b5676e13b7cd222acc63f406a4d62235
Konrad Pazgan <hello@konradpazgan.pl>
2024-01-04 22:43:52 +0100

    Sending files to process in queue

--- src/modules/listing/queue/listing.consumer.ts
+++ src/modules/listing/queue/listing.consumer.ts
@@ -3,17 +3,25 @@ import { Job } from 'bull';
 import { LISTING_QUEUE } from '../../../core/queue/queue.constants';
 import { LoggerService } from '../../../core/logger/logger.service';
 import { BaseConsumer } from '../../../core/queue/base.consumer';
+import { UploadListingImageDto } from '../dto/upload-listing-image.dto';
+import { FileService } from '../../../utilities/file/file.service';
 
 @Processor(LISTING_QUEUE)
 export class ListingConsumer extends BaseConsumer {
-  constructor(logger: LoggerService) {
+  constructor(
+    logger: LoggerService,
+    private readonly fileService: FileService,
+  ) {
     super(logger);
   }
 
   @Process(`createListingImage`)
-  async createListingImage(job: Job<Express.Multer.File>) {
-    console.log(`FILE PROCESSED IN QUEUE:`, job.data.originalname);
-    return job.data;
+  async createListingImage(job: Job<UploadListingImageDto>) {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    // const buffer =
+    this.fileService.base64ToBuffer(job.data.base64File);
+    // TODO: upload file to Google Cloud Storage
+    // TODO: store respective Google Cloud Storage URL in database
   }
 
   @OnQueueFailed()
